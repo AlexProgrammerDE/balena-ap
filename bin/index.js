7504ce1a07dc0064e6b6f5ee999890a86662476e
AlexProgrammerDE <abc123-50@gmx.de>
2020-02-10 13:14:17 +0100

    Change files

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,26 @@
 #! /usr/bin/env node
 
+var hostapd = require('wireless-tools/hostapd');
+
 var path = require('path');
 var express = require('express');
 var app = express();
 
 var dir = path.join(__dirname, 'public');
+
+var options = {
+  channel: 6,
+  driver: 'rtl871xdrv',
+  hw_mode: 'g',
+  interface: 'wlan0',
+  ssid: 'RaspberryPi',
+  wpa: 2,
+  wpa_passphrase: 'raspberry'
+};
+ 
+hostapd.enable(options, function(err) {
+  console.log('Device is discoverable.')
+});
 
 app.use(express.static(dir));
 
